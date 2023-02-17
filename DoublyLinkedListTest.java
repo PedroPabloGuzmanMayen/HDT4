53fc5908afa5a820e79525f0d828ac6eb7c6fbf6
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-02-17 11:56:41 -0600

    Update DoublyLinkedListTest.java
    Se añadieron más tests

--- DoublyLinkedListTest.java
+++ DoublyLinkedListTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,19 @@ public class DoublyLinkedListTest {
         doublyLinkedList.addLast(2);
         doublyLinkedList.addLast(3);
         assertEquals(2, doublyLinkedList.remove(1));
+    }
+
+    @Test
+    public void addTest() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<Integer>();
+        doublyLinkedList.addLast(1);
+        doublyLinkedList.addLast(2);
+        assertEquals(2, doublyLinkedList.get(doublyLinkedList.size() - 1));
+    }
+
+    @Test
+    public void isEmptyTest() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<Integer>();
+        assertTrue(doublyLinkedList.isEmpty());
     }
 }

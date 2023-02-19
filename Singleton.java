1dc2cd88bffb143efc7a7679ab7b43654d306410
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-02-19 16:12:05 -0600

    Correcciones

---
+++ Singleton.java
@@ -0,0 +1,42 @@
+/*
+ * Autor: Alejandro Anton, Alejandro Jose, Pedro pablo guzman
+ * hoja de trabajo 4
+ * deseño siglenton
+ */
+public class Singleton {
+    
+    private static Singleton singleInstance;
+
+    private Calculator calculator;
+
+    /**
+     * Constructor de Singleton
+     */
+    private Singleton() {
+        this.calculator = new Calculator();
+    }
+
+    /**
+     * Devuelve la instancia de la clase a la que se le aplica el diseño Singleton
+     * 
+     * @return Instancia de Singleton
+     */
+    public static Singleton getInstance(){
+
+        if(singleInstance == null){
+            singleInstance = new Singleton();
+        }
+        /*indica que si singleInstance no tiene valor que se le asigne un nuevo valor en calculadora*/
+        return singleInstance;
+    }
+
+    /**
+     * Obtiene la instancia de única Calculator guardada en Singleton
+     * 
+     * @return Instancia de Calculator
+     */
+    public Calculator geCalculator() {
+        return this.calculator;
+    }
+
+}
